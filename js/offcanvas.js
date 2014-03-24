35b9575bb9e9311848edd53ef89b0f152175d5b9
SimonBriche <sbriche@free.fr>
2014-03-24 10:26:40 +0100

    modal window update

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -18,10 +18,12 @@ $(document).ready(function () {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-                $('html,body').animate({
-                     scrollTop: target.offset().top - parseInt($('.container-fluid.main').css("padding-top"))
-                }, 500);
-                $('[data-toggle=offcanvas]').click();
+				$('[data-toggle=offcanvas]').click();
+				setTimeout(function() {
+					$('html,body').animate({
+						 scrollTop: target.offset().top - parseInt($('.container-fluid.main').css("padding-top"))
+					}, 500);
+				},$('[data-toggle=offcanvas]').is(':visible') ? 150 : 0);
                 return false;
             }
         }
